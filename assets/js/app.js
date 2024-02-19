36b07b8e2f0c085d97a43e04b8b5f4cd2e60310e
Ritik <rvjaat25@gmail.com>
2024-02-19 16:19:10 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+let mybutton = document.getElementById("top-button")
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block"
+  } else {
+      mybutton.style.display = "none"
+  }
+};
+
+function topfuncition() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+function openNav() {
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
+}
+
+const Preload = document.getElementById("preload")
+setTimeout(() => {
+  document.body.classList.remove("overflow-hidden");
+  Preload.classList.add("d-none")
+}, "4300");
+
+window.onscroll = function () { scrollFunction() };
+
